775920f2f480f0128906f5ae92fc6b6b0c92b6c6
Dmitriy Monakhov <dimamon6032403@gmail.com>
2025-12-24 16:59:19 +0100

    Add project files.

---
+++ DiceCalculator.cpp
@@ -0,0 +1,12 @@
+﻿// DiceCalculator.cpp : Defines the entry point for the application.
+//
+
+#include "DiceCalculator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ src/DiceCalculator/Dummy.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "CityDraft/Angles.h"
+
+namespace DiceCalculator
+{
+	TEST(DummyTest, DummyTest1)
+	{
+		
+	}
+
+	
+}
